bfe2f0dbbbcb43425dab5db3f277e3fd4ec80400
adebowale <ademibowaleadegboye@gmail.com>
2022-10-20 11:57:12 +0100

    initialise post functionality

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+          has_many :posts
+          has_many :comments
+          has_many :likes
+        
+          def recent_posts
+            posts.order(created_at: :desc).last(3)
+          end
+        end
+        
