4ae2bdec8b9516adedfc42224c58a9dfd1ea41ff
adebowale <ademibowaleadegboye@gmail.com>
2022-10-20 12:09:18 +0100

    fix linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ApplicationRecord
-   belongs_to :user
-   has_many :Comments       
-   has_many :likes_counter
+  belongs_to :user
+  has_many :Comments
+  has_many :likes_counter
 
-   after_save :updates_posts_counter
+  after_save :updates_posts_counter
 
   def updates_posts_counter
     user.increment!(:PostsCounter)
