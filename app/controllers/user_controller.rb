fa8b4b2d0f178e6350318a969b7f004f23e97289
adebowale <ademibowaleadegboye@gmail.com>
2022-10-26 09:54:19 +0100

    setup blog form

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,9 +1,9 @@
-class UsersController < ApplicationRecord
+class UsersController < ApplicationController
   def index
-    @user = User.all
+    @users = User.all
   end
 
   def show
-    @user = User.create(param[:id])
+    @user = User.find(params[:id].to_i)
   end
 end
