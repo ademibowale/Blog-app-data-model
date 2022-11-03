430f8c3ccc295dedc977a953b2fdfc7902568145
adebowale <ademibowaleadegboye@gmail.com>
2022-11-03 11:05:40 +0100

    implement the show post and post counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,11 +9,14 @@ class Post < ApplicationRecord
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def updates_posts_counter
-    user.increment!(:PostsCounter)
-  end
 
   def recent_comments
     comments.order(created_at: :desc).last(5)
+  end
+
+  private
+  
+  def updates_posts_counter
+    user.increment!(:PostsCounter)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,15 @@
 # frozen_string_literal: true
 
-Rails.application.routes.draw do
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :create, :new] do
-      resources :comments, only: [:create, :new]
-        resources :likes, only: [:create]
-    end
-  end
-    root to: 'post#index'
-
-
-   
+Rails.application.routes.draw do   
   # get /users to controler to action 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root "users#index"
+
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
