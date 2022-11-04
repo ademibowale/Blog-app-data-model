c6497ef0470d513e4cb3f7bcdf257093d5ed9cbc
adebowale <ademibowaleadegboye@gmail.com>
2022-11-04 15:27:45 +0100

    add local config and creat devise for view

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,17 @@ Rails.application.routes.draw do
 
   root "users#index"
 
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :create, :new] do
+    resources :comments, only: [:create, :new]
+    resources :likes, only: [:create]
+    resources :comments, only: [:index, :show]
+    resources :likes, only: [:index, :show]
+
+    
+
+
+
+
   end
 end
