3f63737c1b769c5ca9d956a05ba76036fe9594aa
adebowale <ademibowaleadegboye@gmail.com>
2022-11-05 20:38:10 +0100

    Add the api authentication to the blog app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts
@@ -8,7 +10,17 @@ class User < ApplicationRecord
   validates :Name, presence: true
   validates :PostsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  before_save :assign_role
+  
   def recent_posts
     posts.order(created_at: :desc).last(3)
+  end
+
+  def admin?
+    role == 'admin'
+  end
+
+  def assign_role
+    self.role = 'user' if role.nil?
   end
 end
