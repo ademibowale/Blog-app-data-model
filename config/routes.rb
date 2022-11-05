38dcbd55356846699fcd931dd6db5b804563bc6e
adebowale <ademibowaleadegboye@gmail.com>
2022-11-05 15:12:53 +0100

    add comfirmiable

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do   
-  devise_for :model_1s
+  devise_for :users
   # get /users to controler to action 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
